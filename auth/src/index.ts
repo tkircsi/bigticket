f0eb3561ce5e82f04ecb1628c32e29cd88e29564
Tibor Kircsi <tkircsi@gmail.com>
2020-05-25 13:16:53 +0200

    implement signup route and add error handling middleware

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,13 +1,21 @@
 import express from 'express';
 import morgan from 'morgan';
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handler';
 
 const app = express();
 app.use(express.json());
 app.use(morgan('dev'));
 
-app.get('/api/users/currentuser', (req, res) => {
-  res.send('Health check!');
-});
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log('Auth service is listening on port 3000...!!!!!');
