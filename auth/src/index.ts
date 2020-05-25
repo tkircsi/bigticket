bae2b56f6fbb16e697abea8fabd56d04c7d1c23c
Tibor Kircsi <tkircsi@gmail.com>
2020-05-25 15:27:28 +0200

    response normalization and custom error handling

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,10 +1,13 @@
 import express from 'express';
+import 'express-async-errors';
 import morgan from 'morgan';
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
 import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
+import { asyncHandler } from './middlewares/async';
 
 const app = express();
 app.use(express.json());
@@ -15,8 +18,12 @@ app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 
+app.get('*', async (req, res, next) => {
+  throw new NotFoundError();
+});
+
 app.use(errorHandler);
 
 app.listen(3000, () => {
-  console.log('Auth service is listening on port 3000...!!!!!');
+  console.log('Auth service is listening on port 3000');
 });
