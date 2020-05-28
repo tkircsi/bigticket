e095704936a9d5f8bf4e8c9c75696617f9ba0c7b
Tibor Kircsi <tkircsi@gmail.com>
2020-05-28 10:48:06 +0200

    current user and auth middleware

---
+++ auth/src/errors/not-authorized-error.ts
@@ -0,0 +1,14 @@
+import { CustomError } from './custom-error';
+
+export class NotAuthorizedError extends CustomError {
+  statusCode = 401;
+  constructor(message?: string) {
+    super(message);
+
+    Object.setPrototypeOf(this, NotAuthorizedError.prototype);
+  }
+
+  serializeErrors() {
+    return [{ message: this.message }];
+  }
+}
