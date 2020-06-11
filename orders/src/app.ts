47a8961ef7bd380142493e3e61b6f1b5eed517e1
Tibor Kircsi <tkircsi@gmail.com>
2020-06-11 12:36:31 +0200

    initial setup of orders service

---
+++ orders/src/app.ts
@@ -0,0 +1,35 @@
+import express from 'express';
+import 'express-async-errors';
+import morgan from 'morgan';
+import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError, currentUser } from '@bigticket/common';
+
+// import { createTicketRouter } from './routes/new';
+// import { showTicketRouter } from './routes/show';
+// import { indexTicketRouter } from './routes/index';
+// import { updateTicketRouter } from './routes/update';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  })
+);
+
+app.use(currentUser);
+// app.use(createTicketRouter);
+// app.use(showTicketRouter);
+// app.use(indexTicketRouter);
+// app.use(updateTicketRouter);
+
+app.get('*', async (req, res, next) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
