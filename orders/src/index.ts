c0b5997a5e9df4a7b0cb5a965f4d64143889c12b
Tibor Kircsi <tkircsi@gmail.com>
2020-06-15 10:26:48 +0200

    implement orders service listeners

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
 import { app } from './app';
 import { natsWrapper } from './nats-wrapper';
+import { TicketCreatedListener } from './events/listeners/ticket-created-listener';
+import { TicketUpdatedListener } from './events/listeners/ticket-updated-listener';
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
@@ -37,6 +39,11 @@ const start = async () => {
     process.on('SIGTERM', () => natsWrapper.client.close());
 
     console.log('Connecting to bigticket NATS service successfully...');
+
+    new TicketCreatedListener(natsWrapper.client).listen();
+    console.log('Listening for TicketCreatedEvent...');
+    new TicketUpdatedListener(natsWrapper.client).listen();
+    console.log('Listening for TicketUpdatedEvent...');
 
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
