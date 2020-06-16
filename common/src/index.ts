5fee951018ea79655490e6e9c7c1a734015a8fbf
Tibor Kircsi <tkircsi@gmail.com>
2020-06-16 15:06:32 +0200

    add expiration complete event

--- common/src/index.ts
+++ common/src/index.ts
@@ -23,3 +23,5 @@ export * from './events/types/order-status';
 
 export * from './events/order-created-event';
 export * from './events/order-cancelled-event';
+
+export * from './events/expiration-complete-event';
