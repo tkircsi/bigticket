ead9777dd5d408507495909071e3082ce4fd9fb2
Tibor Kircsi <tkircsi@gmail.com>
2020-06-18 23:16:25 +0200

    finished payments and orders services

---
+++ orders/src/events/listeners/payment-created-listener.ts
@@ -0,0 +1,31 @@
+import { Message } from 'node-nats-streaming';
+import {
+  Subjects,
+  Listener,
+  PaymentCreatedEvent,
+  OrderStatus,
+  NotFoundError,
+} from '@bigticket/common';
+import { queueGroupName } from './queue-group-name';
+import { Order } from '../../models/order';
+
+export class PaymentCreatedListener extends Listener<PaymentCreatedEvent> {
+  readonly subject = Subjects.PaymentCreated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: PaymentCreatedEvent['data'], msg: Message) {
+    const { orderId } = data;
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    order.set({
+      status: OrderStatus.Complete,
+    });
+    await order.save();
+
+    msg.ack();
+  }
+}
