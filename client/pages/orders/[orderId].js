c1c450304aabe0248dcf9f965865a212f835cebf
Tibor Kircsi <tkircsi@gmail.com>
2020-06-19 15:39:36 +0200

    add ticket order and payment pages to client

---
+++ client/pages/orders/[orderId].js
@@ -0,0 +1,55 @@
+import useRequest from '../../hooks/use-request';
+import { useEffect, useState } from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const OrderShow = ({ order, currentUser }) => {
+  const [timeLeft, setTimeLeft] = useState(0);
+  const [doRequest, errors] = useRequest({
+    url: '/api/payments',
+    method: 'post',
+    body: {
+      orderId: order.id,
+    },
+    onSuccess: (payment) => console.log(payment),
+  });
+
+  useEffect(() => {
+    const findTimeLeft = () => {
+      const msLeft = new Date(order.expiresAt) - new Date();
+      setTimeLeft(Math.round(msLeft / 1000));
+    };
+
+    findTimeLeft();
+    const timerId = setInterval(findTimeLeft, 1000);
+
+    return () => {
+      clearInterval(timerId);
+    };
+  }, [order]);
+
+  if (timeLeft < 0) {
+    return <div>Order expired.</div>;
+  }
+
+  return (
+    <div>
+      Time left to pay: {timeLeft} seconds.
+      <StripeCheckout
+        token={({ id }) => doRequest({ token: id })}
+        stripeKey="pk_test_51GvMMBF92SM8mhjwIGc8AHpKYlvTJO5eKuBAGJZYaL0i5c4BJJJUqTYlQx7v4hr7C1K9cjR8ydfkCsUTD63pUYiX00uD5BlZ4L"
+        amount={order.ticket.price * 100}
+        email={currentUser.email}
+      />
+      {errors}
+    </div>
+  );
+};
+
+OrderShow.getInitialProps = async (context, client) => {
+  const { orderId } = context.query;
+  const { data } = await client.get(`/api/orders/${orderId}`);
+
+  return { order: data };
+};
+
+export default OrderShow;
